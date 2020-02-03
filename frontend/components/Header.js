5fb419203d26d91bc3b8c50d7daf715376aaa3af
claire froelich <claire.froelich@gmail.com>
2020-02-03 12:14:26 -0500

    init

---
+++ frontend/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = (props) => (
+  <div>
+    <div className='bar'>
+      <a href="">SickFits</a>
+    </div>
+    <div className='subbar'>
+      <p>Search</p>
+    </div>
+  </div>
+);
+
+export default Header;
---
+++ frontend/pages/sell.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Sell = props => (
+  <div>
+    <div>Sell!</div>
+    <Link href="/">
+      <a>Home</a>
+    </Link>
+  </div>
+);
+
+export default Sell;
