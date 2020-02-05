1bded75e196d36cf87639a8c5043af797555a19f
claire froelich <claire.froelich@gmail.com>
2020-02-05 09:51:09 -0500

    config graphql-yoga:

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,23 @@
-// let's go!
+// entry point to our app, so get the ENV variables
+require('dotenv').config({path: 'variables.env'});
+
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// TODO: use express middleware to handle cookies (JWT)
+// TODO: use express middle to populate curr user
+
+server.start({
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    },
+    port: 4009,
+  }, deets => {
+      console.log(`Server is now running on https://localhost:${deets.port}`);
+    }
+);
+
+// cors above only allows db access from authorized url (defined in ENV)
