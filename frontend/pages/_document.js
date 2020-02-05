2846d122b2056d1b631ae80565debc8c8f28a8ac
claire froelich <claire.froelich@gmail.com>
2020-02-04 19:08:03 -0500

    config prisma

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -5,6 +5,7 @@ import { ServerStyleSheet } from 'styled-components';
 export default class MyDocument extends Document {
   static getInitialProps({ renderPage }) {
     const sheet = new ServerStyleSheet();
+    // get all the 'styled' parts of styled components
     const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
     const styleTags = sheet.getStyleElement();
     return { ...page, styleTags };
