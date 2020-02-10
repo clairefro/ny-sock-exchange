9190efbdbd71a50eadc342b00b15307beae7be88
claire froelich <claire.froelich@gmail.com>
2020-02-10 10:48:58 -0500

    establish item-user dta relationship

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -17,10 +17,17 @@ const signTokenAndSetCookie = (user, ctx) => {
 const mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: check if user logged in
-
+    if(!ctx.request.userId) {
+      throw new Error('You must be logged in to do that.');
+    }
     // here we interact with prisma db
      const item = await ctx.db.mutation.createItem({
       data: {
+        user: {
+          connect: {
+            id: ctx.request.userId
+          }
+        },
         ... args
       }
     }, info);
