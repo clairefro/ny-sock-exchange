22638683384a4ba0c17000c8ae68a2358cef0c5e
claire froelich <claire.froelich@gmail.com>
2020-02-11 10:35:44 -0500

    add toggle cart mutation and interface

--- frontend/components/styles/NavStyles.js
+++ frontend/components/styles/NavStyles.js
@@ -13,6 +13,8 @@ const NavStyles = styled.ul`
     align-items: center;
     position: relative;
     text-transform: uppercase;
+    font-family: inherit;
+    color: inherit;
     font-weight: 900;
     font-size: 1em;
     background: none;
--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -1,6 +1,9 @@
 import withApollo from 'next-with-apollo';
 import ApolloClient from 'apollo-boost';
 import { endpoint } from '../config';
+import { LOCAL_STATE_QUERY } from '../components/Cart';
+import { TOGGLE_CART_MUTATION } from '../components/Cart';
+
 
 function createClient({ headers }) {
   return new ApolloClient({
@@ -12,6 +15,28 @@ function createClient({ headers }) {
         },
         headers,
       });
+    },
+
+    // local data
+    clientState: {
+      resolvers: {
+        Mutation: {
+          toggleCart(_, variables, { cache }) {
+            // read cartOpen value from cache
+            const { cartOpen } = cache.readQuery({
+              query: LOCAL_STATE_QUERY,
+            });
+            const data = {
+              data: { cartOpen: !cartOpen }
+            };
+            cache.writeData(data);
+            return data;
+          }
+        }
+      },
+      defaults: {
+        cartOpen: false,
+      },
     },
   });
 }
