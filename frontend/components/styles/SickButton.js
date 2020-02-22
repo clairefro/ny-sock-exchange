975566fcd77690c41f34285eb6d6ded918867d1e
claire froelich <claire.froelich@gmail.com>
2020-02-21 21:12:17 -0500

    make sick button sicker

--- frontend/components/styles/SickButton.js
+++ frontend/components/styles/SickButton.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const SickButton = styled.button`
   background: red;
   color: white;
-  font-weight: 500;
+  font-weight: 900;
   border: 0;
   border-radius: 0;
   text-transform: uppercase;
