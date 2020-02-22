7b2d2fb454bb6dc71a8de967ee690a7e94090fe7
claire froelich <claire.froelich@gmail.com>
2020-02-22 10:43:31 -0500

    add item show links to cart items

--- frontend/components/CartItem.js
+++ frontend/components/CartItem.js
@@ -1,20 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import Link from 'next/link';
 import RemoveFromCart from './RemoveFromCart';
 import formatMoney from '../lib/formatMoney';
 
 const CartItemStyles = styled.li`
-  padding: 1rem 0;
-  border-bottom: 1px solid ${props => props.theme.lightgrey};
-  display: grid;
-  align-items: center;
-  grid-template-columns: auto 1fr auto;
-  img {
-    margin-right: 10px;
-  }
-  h3,p {
-    margin: 0;
+  a {
+    padding: 1rem 0;
+    border-bottom: 1px solid ${props => props.theme.lightgrey};
+    display: grid;
+    align-items: center;
+    grid-template-columns: auto 1fr auto;
+    img {
+      margin-right: 10px;
+    }
+    h3,p {
+      margin: 0;
+    }
   }
 `;
 
@@ -28,22 +31,30 @@ const CartItem = ({ cartItem }) => {
   );
   return (
     <CartItemStyles>
-    <img
-      src={cartItem.item.image}
-      alt={cartItem.item.title}
-      width="100px"
-      />
-      <div className="cart-item-details">
-        <h3>{cartItem.item.title}</h3>
-        <p>
-          {formatMoney(cartItem.item.price * cartItem.quantity)}
-          {' - '}
-          <em>
-            {cartItem.quantity} &times; {formatMoney(cartItem.item.price)} ea.
-          </em>
-          </p>
-      </div>
-      <RemoveFromCart id={cartItem.id} />
+    <Link href={{
+      pathname: '/item',
+      query: { id: cartItem.item.id}
+    }}>
+     <a>
+       <img
+         src={cartItem.item.image}
+         alt={cartItem.item.title}
+         width="100px"
+         />
+       <div className="cart-item-details">
+         <h3>{cartItem.item.title}</h3>
+         <p>
+           {formatMoney(cartItem.item.price * cartItem.quantity)}
+           {' - '}
+           <em>
+             {cartItem.quantity} &times; {formatMoney(cartItem.item.price)} ea.
+           </em>
+           </p>
+       </div>
+       <RemoveFromCart id={cartItem.id} />
+     </a>
+
+      </Link>
     </CartItemStyles>
   );
 }
