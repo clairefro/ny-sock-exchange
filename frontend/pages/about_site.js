45bbaa05fb625af475fd2afa7912846781f1d064
claire froelich <claire.froelich@gmail.com>
2020-02-25 09:51:40 -0500

    add tbody to table, testing table

--- frontend/pages/about_site.js
+++ frontend/pages/about_site.js
@@ -28,44 +28,64 @@ const AboutSitePage = props => (
     <p>If you really want to sell your socks, try <a href="https://www.craigslist.org/about/sites" target="_blank">CraigsList</a>.</p>
     <p>Or enjoy a fake shopping spree here with my credit card: 4242 4242 4242 4242 (exp. 2/22, sec. code: 222)</p>
     <p>This site was built to practice React and GraphQL principles taught in <a href="https://advancedreact.com/">Wes Bos' Advanced React Course</a>. </p>
+    <p>See the full code mono-repo <a href="https://github.com/clairefro/ny-sock-exchange">here</a>.</p>
     <div className="stack">
       <table>
-        <tr>
-          <th colspan="2">Frontend</th>
-        </tr>
-        <tr>
-          <td><img src="../static/icons/react.png" width="100px" alt="react"/></td>
-          <td>React</td>
-        </tr>
-        <tr>
-          <td><img src="../static/icons/next.png" width="100px" alt="next.js"/></td>
-          <td>Next.js</td>
-        </tr>
-        <tr>
-          <td><img src="../static/icons/apollo.png" width="100px" alt="apollo"/></td>
-          <td>Apollo</td>
-        </tr>
-        <tr>
-          <td><img src="../static/icons/styled.png" width="100px" alt="styled components"/></td>
-          <td>Styled Components</td>
-        </tr>
+        <tbody>
+          <tr>
+            <th colSpan="2">Frontend</th>
+          </tr>
+          <tr>
+            <td><img src="../static/icons/react.png" width="100px" alt="react"/></td>
+            <td>React</td>
+          </tr>
+          <tr>
+            <td><img src="../static/icons/next.png" width="100px" alt="next.js"/></td>
+            <td>Next.js</td>
+          </tr>
+          <tr>
+            <td><img src="../static/icons/apollo.png" width="100px" alt="apollo"/></td>
+            <td>Apollo</td>
+          </tr>
+          <tr>
+            <td><img src="../static/icons/styled.png" width="100px" alt="styled components"/></td>
+            <td>Styled Components</td>
+          </tr>
+        </tbody>
       </table>
       <table>
-        <tr>
-          <th colspan="2">Backend</th>
-        </tr>
-        <tr>
-          <td align="center"><img src="../static/icons/gql.png" width="60px" alt="react"/></td>
-          <td>GraphQL Yoga</td>
-        </tr>
-        <tr>
-          <td><img src="../static/icons/prisma.svg" width="100px" alt="prisma"/></td>
-          <td>Prisma</td>
-        </tr>
-        <tr>
-          <td><img src="../static/icons/postgre.png" width="100px" alt="postgresql"/></td>
-          <td>PostgreSQL</td>
-        </tr>
+        <tbody>
+          <tr>
+            <th colSpan="2">Backend</th>
+          </tr>
+          <tr>
+            <td align="center"><img src="../static/icons/gql.png" width="60px" alt="react"/></td>
+            <td>GraphQL Yoga</td>
+          </tr>
+          <tr>
+            <td><img src="../static/icons/prisma.svg" width="100px" alt="prisma"/></td>
+            <td>Prisma</td>
+          </tr>
+          <tr>
+            <td><img src="../static/icons/postgre.png" width="100px" alt="postgresql"/></td>
+            <td>PostgreSQL</td>
+          </tr>
+        </tbody>
+      </table>
+      <table>
+        <tbody>
+          <tr>
+            <th colSpan="2">Testing</th>
+          </tr>
+          <tr>
+            <td><img src="../static/icons/jest.png" width="100px" alt="jest"/></td>
+            <td>Jest</td>
+          </tr>
+          <tr>
+            <td><img src="../static/icons/enzyme.jpeg" width="100px" alt="enzyme"/></td>
+            <td>Enzyme</td>
+          </tr>
+        </tbody>
       </table>
     </div>
 
