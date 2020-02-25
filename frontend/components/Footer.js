7ab482c1fb986bd5ed06a189f0d3e3565152fab3
claire froelich <claire.froelich@gmail.com>
2020-02-24 21:48:50 -0500

    footer tweaks

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -6,29 +6,38 @@ import Router from 'next/router';
 const FooterStyles = styled.div`
   background-color: ${props => props.theme.grey};
   color: ${props => props.theme.offWhite};
+  font-family: Arial;
   padding: 20px;
   margin-top: 60px;
   flex-shrink: 0;
   width: 100%;
   height: 20rem;
   justify-self: flex-end;
-`;
-
-const IconLinks = styled.div`
-  font-size: 3rem;
-  margin-right: 1rem;
+  display: flex;
+  flex-direction: column;
   a {
     color: ${props => props.theme.offWhite};
-    margin-right: 0.5rem;
     &:hover {
       color: ${props => props.theme.red};
     }
   }
 `;
 
+const IconLinks = styled.div`
+  font-size: 3rem;
+  margin-right: 1rem;
+  align-self: center;
+  a {
+    margin-right: 1rem;
+  }
+`;
+
 const Footer = (props) => (
   <FooterStyles>
-    <p>TODO: Write a footer</p>
+  <Link href={{pathname: '/about_site'}}>
+    <a>About this site</a>
+  </Link>
+    <a href="#">About the creator</a>
     <IconLinks>
       <a href="#"><i className="lab la-instagram"></i></a>
       <a href="#"><i className="lab la-facebook-square"></i></a>
